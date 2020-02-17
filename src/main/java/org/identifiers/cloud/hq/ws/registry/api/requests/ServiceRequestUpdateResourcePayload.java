1671b52764162d4ff76619675140293cdfa06d70
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-17 14:30:47 +0000

    resource request update request payload data model, placeholderç

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestUpdateResourcePayload.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2020-02-17 14:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Data model for requests on updating resources
+ */
+public class ServiceRequestUpdateResourcePayload {
+    // TODO
+}
