c605e4071225893e2c6ae1b4ec9806514cf934e6
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-17 14:46:47 +0000

    service request for update resource api

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestUpdateResource.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2020-02-17 14:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestUpdateResource extends ServiceRequest<ServiceRequestUpdateResourcePayload> {
+    
+}
