63329b550fb2530d648c1228ffcb7aaed650358d
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:28:53 +0000

    resource update session, entity annotations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSession.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSession.java
@@ -1,5 +1,16 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -7,9 +18,18 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
- *
+ * <p>
  * This model represents a resource update session, it groups together the different stages in the process of approving
  * or rejecting a resource update request, and the evolution of its content
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class ResourceUpdateSession {
+    @Id
+    @GeneratedValue
+    private long id;
 }
