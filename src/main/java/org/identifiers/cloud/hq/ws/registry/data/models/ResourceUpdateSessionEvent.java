ba83758e6028c5ccb186cab1d9e2c844257c8582
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:51:36 +0000

    resource update session event, root data model definition

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSessionEvent.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSessionEvent.java
@@ -1,5 +1,16 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -10,5 +21,42 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  *
  * Root of the data model tree that represents the events that are part of a resource update request session.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@EntityListeners(AuditingEntityListener.class)
 public class ResourceUpdateSessionEvent {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    // This part is interpreted mainly on the client side
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String eventName;
+
+    // This attribute should contain information on who triggered / created a particular event (it will usually be the
+    // 'curator' that's working on the associated prefix registration session)
+    @Column(nullable = false)
+    private String actor;
+
+    // Additional information on this event is useful, but optional
+    private String additionalInformation;
+
+    // Link to the resource update session this event belongs to
+    @ManyToOne(optional = false)
+    private ResourceUpdateSession resourceUpdateSession;
+
+    // Resource update request that may be associated with a specialised instance of this event
+    @ManyToOne
+    private ResourceUpdateRequest resourceUpdateRequest;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date created;
 }
