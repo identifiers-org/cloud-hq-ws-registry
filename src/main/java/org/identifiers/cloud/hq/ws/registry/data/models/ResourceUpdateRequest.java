2e517e10a8bc53837099b7c94ed689a546329a24
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:25:18 +0000

    resource update request persistence data model completed

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
@@ -1,13 +1,15 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-import org.identifiers.cloud.hq.ws.registry.api.data.models.Requester;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Project: cloud-hq-ws-registry
@@ -30,7 +32,8 @@ public class ResourceUpdateRequest {
     @GeneratedValue
     private long id;
 
-    // Registry internal ID for the resource
+    // Registry internal ID for the resource, it is mandatory to provide a reference to an existing resource, on which
+    // an update is being requested
     @ManyToOne(optional = false)
     private Resource resource;
 
@@ -49,39 +52,58 @@ public class ResourceUpdateRequest {
     @Column(length = 2000)
     private String providerDescription;
 
-    // Location Associated with the provider being updated
+    // Location Associated with the provider being updated, it references a supported location in the registry, but the
+    // attribute is optional as it may not be the subject of the request
     private String providerLocation;
 
-    // Unique identifier for this provider within the namespace for provider selection when resolving compact identifiers
+    // Unique identifier for this provider within the namespace for provider selection when resolving compact
+    // identifiers, optional, as it may not be part of the update request
     private String providerCode;
 
-    // URL pattern for the provider being updated
+    // URL pattern for the provider being updated, optional, as it may not be part of the update request
+    @Column(length = 2000)
     private String providerUrlPattern;
 
-    // This is a sample LUI that is covered by the resource being registered
+    // This is a sample LUI that is covered by the resource being registered, optional, as it may not be part of the update request
     private String sampleId;
 
     // --- Associated Institution ---
-    // This is the name of the institution that owns the resource that's being updated
+    // This is the name of the institution that owns the resource that's being updated, optional, as it may not be part of the update request
     private String institutionName;
 
-    // Home URL for the institution that operates the resource being updated
+    // Home URL for the institution that operates the resource being updated, optional, as it may not be part of the update request
+    @Column(length = 2000)
     private String institutionHomeUrl;
 
-    // A description related to the institution that operates the resource that's being updated
+    // A description related to the institution that operates the resource that's being updated, optional, as it may not be part of the update request
+    @Column(length = 2000)
     private String institutionDescription;
 
-    // Location of the institution that owns the resource that's being updated
+    // Location of the institution that owns the resource that's being updated, optional, as it may not be part of the update request
     private String institutionLocation;
 
-    // ROR ID for this institution
+    // ROR ID for this institution, optional, as it may not be part of the update request
     private String institutionRorId;
 
     // --- UPDATE REQUEST Details ---
+    // It is not mandatory to provide additional information attached to the resource update request
+    @Column(length = 4000)
     private String additionalInformation;
-    private Requester requester;
+
+    // Information about the contact details for the requester is required
+    @Column(nullable = false)
+    private String requesterName;
+    @Column(nullable = false)
+    private String requesterEmail;
+
     // Other request action types
     private boolean actionTogglePrimaryFlag = false;
     private boolean actionDeprecateResource = false;
     private boolean actionContactPersonUpdate = false;
+
+    // --- Additional Details on the request
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date created;
 }
