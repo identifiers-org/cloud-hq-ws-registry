a76c7ae6991d85e6b6ac222d7e587b9f8a0c12b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 10:23:03 +0000

    resource update request persistence data model, model attributes

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.Requester;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
@@ -31,4 +32,56 @@ public class ResourceUpdateRequest {
     @Id
     @GeneratedValue
     private long id;
+
+    // Registry internal ID for the resource
+    private long providerId;
+
+    // --- Updatable Provider details ---
+    // Resource MIR ID
+    private String providerMirId;
+
+    // Home URL for the resource
+    private String providerHomeUrl;
+
+    // Name update for the provider
+    private String providerName;
+
+    // Description for the provider being updated
+    private String providerDescription;
+
+    // Location Associated with the provider being updated
+    private String providerLocation;
+
+    // Unique identifier for this provider within the namespace for provider selection when resolving compact identifiers
+    private String providerCode;
+
+    // URL pattern for the provider being updated
+    private String providerUrlPattern;
+
+    // This is a sample LUI that is covered by the resource being registered
+    private String sampleId;
+
+    // --- Associated Institution ---
+    // This is the name of the institution that owns the resource that's being updated
+    private String institutionName;
+
+    // Home URL for the institution that operates the resource being updated
+    private String institutionHomeUrl;
+
+    // A description related to the institution that operates the resource that's being updated
+    private String institutionDescription;
+
+    // Location of the institution that owns the resource that's being updated
+    private String institutionLocation;
+
+    // ROR ID for this institution
+    private String institutionRorId;
+
+    // --- UPDATE REQUEST Details ---
+    private String additionalInformation;
+    private Requester requester;
+    // Other request action types
+    private boolean actionTogglePrimaryFlag = false;
+    private boolean actionDeprecateResource = false;
+    private boolean actionContactPersonUpdate = false;
 }
