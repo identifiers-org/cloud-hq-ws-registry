2c91d35a6c3141a1612e6b2312c15b204602552b
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:27:27 +0000

    resource update session, placeholder and description

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSession.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2020-02-26 12:25
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This model represents a resource update session, it groups together the different stages in the process of approving
+ * or rejecting a resource update request, and the evolution of its content
+ */
+public class ResourceUpdateSession {
+}
