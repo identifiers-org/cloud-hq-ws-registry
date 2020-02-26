c6aa3c5d6f5f4d5b1ba5d1dcb16bd2df745bf1c6
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 10:21:40 +0000

    resource update request persistence data model, model annotations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
@@ -1,5 +1,14 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -7,8 +16,15 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
- *
+ * <p>
  * This is the persistence data model for update requests on resources (providers) in the registry.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class ResourceUpdateRequest {
+
 }
