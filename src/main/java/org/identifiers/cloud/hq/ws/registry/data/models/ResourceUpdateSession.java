0506d3239898d0b3219e5d3e7f1ef8d2a4b3ceeb
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:34:06 +0000

    resource update session, entity definition

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSession.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSession.java
@@ -1,15 +1,15 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Project: cloud-hq-ws-registry
@@ -32,4 +32,18 @@ public class ResourceUpdateSession {
     @Id
     @GeneratedValue
     private long id;
+
+    // This attribute flags whether this session is closed or active
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private boolean closed = false;
+
+    // Latest version of the resource update request associated with this session
+    @OneToOne(optional = false)
+    private ResourceUpdateRequest resourceUpdateRequest;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date created;
 }
