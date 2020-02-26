3fcfc9d8932da49cfb3b89e0385cb0b7617cf889
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 12:46:14 +0000

    resource update session event, root data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateSessionEvent.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2020-02-26 12:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Root of the data model tree that represents the events that are part of a resource update request session.
+ */
+public class ResourceUpdateSessionEvent {
+}
