1f74a100c8ac3ed1d413195057b5fb4f0b9cde88
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-02-26 10:20:19 +0000

    resource update request persistence model, description

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceUpdateRequest.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2020-02-26 10:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is the persistence data model for update requests on resources (providers) in the registry.
+ */
+public class ResourceUpdateRequest {
+}
