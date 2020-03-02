811214eb681b90192892671875eadcc2e1cb135e
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-02 16:47:40 +0000

    placeholder for validator on update contact person

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorActionContactPersonUpdate.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2020-03-02 16:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceUpdateRequestValidatorActionContactPersonUpdate implements ResourceUpdateRequestValidator {
+    // TODO
+}
