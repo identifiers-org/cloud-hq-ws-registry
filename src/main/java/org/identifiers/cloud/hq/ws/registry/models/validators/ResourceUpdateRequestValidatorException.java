d181ee10e31a1b3df7d54f1225227a41117bc5bf
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-02 16:04:31 +0000

    ResourceUpdateRequestValidatorException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2020-03-02 16:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceUpdateRequestValidatorException extends RuntimeException {
+    public ResourceUpdateRequestValidatorException(String message) {
+        super(message);
+    }
+}
