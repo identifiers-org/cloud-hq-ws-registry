19c1f144e4c2cce19785f724212797ea8e4b3720
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-02 16:51:07 +0000

    placeholder for validator on sample ID

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorSampleId.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2020-03-02 16:42
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceUpdateRequestValidatorSampleId implements ResourceUpdateRequestValidator {
+    // TODO
+}
