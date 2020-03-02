445220234a914fe2c72240e4156922439b072672
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-02 15:29:19 +0000

    repository for resource update session event start

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceUpdateSessionEventStartRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.ResourceUpdateSessionEventStart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2020-03-02 15:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceUpdateSessionEventStartRepository extends JpaRepository<ResourceUpdateSessionEventStart, Long> {
+    // TODO
+}
