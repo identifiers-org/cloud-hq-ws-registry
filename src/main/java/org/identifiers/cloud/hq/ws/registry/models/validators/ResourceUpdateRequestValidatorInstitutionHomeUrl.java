d7e3636915a173b3e0079c3e436932ca76c4c065
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 10:49:03 +0000

    resource update request validator, attribute - institution home URL

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionHomeUrl.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionHomeUrl.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -10,4 +15,20 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  */
 public class ResourceUpdateRequestValidatorInstitutionHomeUrl implements ResourceUpdateRequestValidator {
     // TODO
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorInstitutionHomeUrl")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // In a resource update request, providing a home URL for the associated institution is optional as it may not be
+    // the subject of the request. If provided, this validator will only check this attribute in itself, not request wide.
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getInstitutionHomeUrl() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("INVALID Institution Home URL, '%s', '%s'", request.getInstitutionHomeUrl(), e.getMessage()));
+        }
+    }
 }
