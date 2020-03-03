fab26158376df5f1a35ddccb06fffd4650b5eb9a
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 10:28:15 +0000

    resource update request validator, attribute - institution description

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionDescription.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionDescription.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -10,4 +15,25 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  */
 public class ResourceUpdateRequestValidatorInstitutionDescription implements ResourceUpdateRequestValidator {
     // TODO
+
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorInstitutionDescription")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // In a resource update request, providing a description of the associated institution is optional, as it may not
+    // be the subject of the request. If provided, this validator will only check at 'institutionDescription' attribute
+    // level, e.g. it will only check that the provided content for institution description is compliant with the
+    // guidelines on what this attribute should contain, but it won't check beyond this point to find out, for example,
+    // whether the request makes sense itself, e.g. if institutional description has been provided but nothing else,
+    // it may not be a resource update request, but an institution update request.
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getInstitutionDescription() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("INVALID institution description '%s', '%s'", request.getInstitutionDescription(), e.getMessage()));
+        }
+    }
 }
