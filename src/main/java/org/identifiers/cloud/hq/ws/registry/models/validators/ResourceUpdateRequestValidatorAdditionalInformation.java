35303813a557be8f2920479ffc908261ac08ecbe
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 10:00:25 +0000

    resource update request validator, attribute - additional information

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorAdditionalInformation.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorAdditionalInformation.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +14,20 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorAdditionalInformation")
 public class ResourceUpdateRequestValidatorAdditionalInformation implements ResourceUpdateRequestValidator {
     // TODO
+    @Autowired
+    @Qualifier("prefixRegistrationRequestValidatorAdditionalInformation")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // Attaching additional information to a resource update request is optional, but in case it has been provided, we
+    // follow similar rules to those followed for additional information in prefix registration requests
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        return (request.getAdditionalInformation() == null)
+                || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+    }
 }
