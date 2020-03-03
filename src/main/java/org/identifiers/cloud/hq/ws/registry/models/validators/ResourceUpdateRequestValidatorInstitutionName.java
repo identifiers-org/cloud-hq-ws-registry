018bab4cfbc05c69b1d20b215cf565932b5cfd13
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 10:59:28 +0000

    resource update request validator, attribute - institution name

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionName.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionName.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +14,24 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorInstitutionName")
 public class ResourceUpdateRequestValidatorInstitutionName implements ResourceUpdateRequestValidator {
     // TODO
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorInstitutionName")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // In a resource update request, providing a name for the associated institution is optional, as it may not be the
+    // subject of the request. If provided, this validator will only check this attribute in itself, not request wide.
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getInstitutionName() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("INVALID Institution Name, '%s', '%s'", request.getInstitutionName(), e.getMessage()));
+        }
+    }
 }
