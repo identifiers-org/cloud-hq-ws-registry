7f22821997052ed158bc343643f7a60152831244
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 11:10:52 +0000

    resource update request validator, attribute - institution ROR ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionRorId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionRorId.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +12,18 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorInstitutionRorId")
 public class ResourceUpdateRequestValidatorInstitutionRorId implements ResourceUpdateRequestValidator {
     // TODO
+
+    // In a resource update request, providing a ROR ID for the associated institution is optional, as it may not be the
+    // subject of the request. If provided, this validator will only check this attribute in itself, not request wide.
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        // TODO This validator could rely on the existing ROR ID machinery behind the registry ROR ID API to check
+        //  whether a given ROR ID is valid or not.
+        return true;
+    }
 }
