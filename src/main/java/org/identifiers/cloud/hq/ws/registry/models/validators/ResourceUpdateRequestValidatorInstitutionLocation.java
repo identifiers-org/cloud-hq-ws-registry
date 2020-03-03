b9e7a2d7f78792ea62df82337d4ab4c6e456497c
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-03 10:54:37 +0000

    resource update request validator, attribute - institution location

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionLocation.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorInstitutionLocation.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -10,4 +15,20 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  */
 public class ResourceUpdateRequestValidatorInstitutionLocation implements ResourceUpdateRequestValidator {
     // TODO
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorInstitutionLocation")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // In a resource update request, providing a location for the associated institution is optional, as it may not be
+    // the subject of the request. If provided, this validator will only check this attribute in itself, not request wide.
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getInstitutionLocation() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("INVALID Institution Location, '%s', '%s'", request.getInstitutionLocation(), e.getMessage()));
+        }
+    }
 }
