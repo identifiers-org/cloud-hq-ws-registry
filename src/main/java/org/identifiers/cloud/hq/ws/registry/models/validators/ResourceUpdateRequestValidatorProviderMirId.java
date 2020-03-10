fad557d63118d70a60530d977b5194da59941532
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-10 14:59:51 +0000

    resource update request validator for provider MIR ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorProviderMirId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorProviderMirId.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Pattern;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +14,20 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorProviderMirId")
 public class ResourceUpdateRequestValidatorProviderMirId implements ResourceUpdateRequestValidator {
     // TODO
+    private static final String REGEX_PATTERN_MIR_ID = "^MIR:\\d{8,}$";
+
+    // Including a MIR ID in the resource update request is optional.
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        // We know the format of MIR IDs, so we can actually make sure it conforms to the format
+        if (request.getProviderMirId() != null) {
+            Pattern pattern = Pattern.compile(REGEX_PATTERN_MIR_ID);
+            return pattern.matcher(request.getProviderMirId()).matches();
+        }
+        return true;
+    }
 }
