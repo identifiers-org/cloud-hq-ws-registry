fed48f2ce8eb3b4937ab2f69bb905df4df87b052
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-10 13:09:43 +0000

    resource update request validation strategy interface, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorStrategy.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: cloud-hq-ws-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2020-03-10 13:06
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceUpdateRequestValidatorStrategy extends ResourceUpdateRequestValidator {
+    // TODO
+}
