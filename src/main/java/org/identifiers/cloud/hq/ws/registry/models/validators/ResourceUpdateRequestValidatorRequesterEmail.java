e9a15d57fbbefabd3eacc70d94bbd26a887f3197
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-10 14:59:16 +0000

    resource update request validator for requester email

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorRequesterEmail.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorRequesterEmail.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +14,20 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorRequesterEmail")
 public class ResourceUpdateRequestValidatorRequesterEmail implements ResourceUpdateRequestValidator {
-    // TODO
+    @Autowired
+    @Qualifier("prefixRegistrationRequestValidatorRequesterEmail")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getRequester().getEmail() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("Invalid requester email, '%s', '%s'", request.getRequester().getEmail(), e.getMessage()));
+        }
+    }
 }
