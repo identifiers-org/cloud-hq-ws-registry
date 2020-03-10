f670a21d895aca9685e21d66c9a0d886b95990ee
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-03-10 14:01:57 +0000

    resource update request validator for sample ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorSampleId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceUpdateRequestValidatorSampleId.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestUpdateResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: cloud-hq-ws-registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -8,6 +14,22 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Qualifier("ResourceUpdateRequestValidatorSampleId")
 public class ResourceUpdateRequestValidatorSampleId implements ResourceUpdateRequestValidator {
-    // TODO
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorSampleId")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    // Including a Sample ID in a resource update request is optional, but in case it has been provided, we follow
+    // similar rules to those followed for additional information in prefix registration requests
+    @Override
+    public boolean validate(ServiceRequestUpdateResourcePayload request) throws ResourceUpdateRequestValidatorException {
+        try {
+            return (request.getSampleId() == null)
+                    || (delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request)));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceUpdateRequestValidatorException(String.format("Invalid sample ID: '%s', '%s'", request.getSampleId(), e.getMessage()));
+        }
+    }
 }
